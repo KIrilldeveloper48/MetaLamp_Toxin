2db4d32742eb636b5945b3289fce74c5da24631d
Kirill <developerpopov@yandex.ru>
2021-05-21 12:49:06 +0300

    Подготовил данные для списка цветов

---
+++ src/conts.js
@@ -0,0 +1,9 @@
+export const COLORS_DATA = [
+  {colorName: `Dark Shade 100%`, colorCode: `#1F2041`, id: `main-color`},
+  {colorName: `Dark Shade 75%`, colorCode: `#1F2041`, id: `text-color-primary`},
+  {colorName: `Dark Shade 50%`, colorCode: `#1F2041`, id: `placeholder-color`},
+  {colorName: `Dark Shade 25%`, colorCode: `#1F2041`, id: `text-color-secondary`},
+  {colorName: `Dark Shade 5%`, colorCode: `#1F2041`, id: `transparent-color`},
+  {colorName: `Purple`, colorCode: `#BC9CFF`, id: `primary-color`},
+  {colorName: `Green`, colorCode: `#6FCF97`, id: `secondary-color`},
+];
