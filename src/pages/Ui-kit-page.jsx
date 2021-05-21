fcc2f2bc78dc24ed450872f4d91a37187a4ae996
Kirill <developerpopov@yandex.ru>
2021-05-21 13:46:11 +0300

    Проимпортировал лого

--- src/pages/Ui-kit-page.jsx
+++ src/pages/Ui-kit-page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ColorList from '../components/color-list/Color-list';
-
+import UiKitLogo from '../assets/img/logo/logo-transparent';
 const UiKitPage = () => {
   return (
     <section className="colors-and-types">
       <div>
-        <img src="../assets/img/logo/logo-transparent.svg" alt="Логотип компании" />
+        <img src={UiKitLogo} alt="Логотип компании" />
       </div>
 
       <ColorList />
