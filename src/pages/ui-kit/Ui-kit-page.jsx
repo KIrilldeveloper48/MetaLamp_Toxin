a66dd12188c945ef2996e5e03d1f2f6f72a94eb6
Kirill <developerpopov@yandex.ru>
2021-06-21 13:26:49 +0300

    Добавил чекбоксы на страницу

--- src/pages/ui-kit/Ui-kit-page.jsx
+++ src/pages/ui-kit/Ui-kit-page.jsx
@@ -10,9 +10,21 @@ import UiKitLogo from '../../assets/img/logo/logo-transparent';
 import './ui-kit.scss';
 import NumberField from '../../components/number-field/Number-field';
 import SubscriptionForm from '../../components/subscription-form/Subscription-form';
-import Checkbox from '../../components/App/checkbox/checkbox';
+import Checkbox from '../../components/checkbox/Checkbox';
+import {CheckboxList, DropdownsList} from './../../conts';
 
 const UiKitPage = () => {
+
+  const CheckboxListForUiKit = {
+    features: [
+      {title: `Завтрак`, descr: ``, checked: false},
+      {title: `Письменный стол`, descr: ``, checked: true},
+      {title: `Стул для кормления`, descr: ``, checked: true},
+      {title: `Кроватка`, descr: ``, checked: true},
+      {title: `Телевизор`, descr: ``, checked: false},
+      {title: `Шампунь`, descr: ``, checked: false},
+    ]
+  };
   return (
     <>
       <section className="colors-and-types">
@@ -42,19 +54,19 @@ const UiKitPage = () => {
               label="Text Field"
               styleType="Hover / Focus" />
 
-            <Dropdown placeholder="Сколько гостей" label="Dropdown" />
+            <Dropdown currentList={[``]} placeholder="Сколько гостей" label="Dropdown" />
 
             <NumberField placeholder="ДД.ММ.ГГГГ" label="masked text field" />
           </div>
 
           <div className="form-elements__group-2">
             <div className="form-elements__date-dropdown">
-              <Dropdown placeholder="ДД.ММ.ГГГГ" label="Date dropdown" />
-              <Dropdown placeholder="19.08.2019" label="Date dropdown" />
+              <Dropdown currentList={[``]} placeholder="ДД.ММ.ГГГГ" label="Date dropdown" />
+              <Dropdown currentList={[``]} placeholder="19.08.2019" label="Date dropdown" />
             </div>
 
             <div className="form-elements__filter-date">
-              <Dropdown placeholder="19 авг - 23 авг" label="Fiter date dropdown" />
+              <Dropdown currentList={[``]} placeholder="19 авг - 23 авг" label="Fiter date dropdown" />
             </div>
 
             <div className="form-elements__filter-date">
@@ -64,9 +76,13 @@ const UiKitPage = () => {
           </div>
 
           <div className="form-elements__group-3">
-            <Dropdown placeholder="2 спальни, 2 кровати..." label="dropdown" styleType="Default" mod="ui-kit-default" />
-            <Dropdown placeholder="2 спальни, 2 кровати..." label="dropdown" styleType="Expanded" mod="ui-kit-expanded" />
-            <Checkbox />
+            <Dropdown currentList={[``]} placeholder="2 спальни, 2 кровати..." label="dropdown" styleType="Default" mod="ui-kit-default" />
+            <Dropdown currentList={DropdownsList.DROPDOWN_ROOM_FILTERS} placeholder="2 спальни, 2 кровати..." label="dropdown" styleType="Expanded" mod="ui-kit-expanded" />
+          </div>
+
+          <div className="form-elements__group-4">
+            <Checkbox currentList={CheckboxListForUiKit.features} isExpended={false}/>
+            <Checkbox currentList={CheckboxListForUiKit.features} isExpended={true}/>
           </div>
 
         </div>
